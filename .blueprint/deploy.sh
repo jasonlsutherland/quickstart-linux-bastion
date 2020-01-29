bd601670ffd7dc51afcc2606d122374bab067e70
Jason Sutherland <jason.sutherland@Jason-Sutherlands-MacBook-Pro.local>
2020-01-29 19:31:40 +0000

    Updated referenced template and user param defaults.

--- .blueprint/deploy.sh
+++ .blueprint/deploy.sh
@@ -6,8 +6,6 @@ echo "Submitting CloudFormation Template..."
 
 aws cloudformation create-stack --stack-name ${STACK_NAME} \
 --template-body file://blueprint/templates/linux-bastion-master.template \
---parameters \
-	ParameterKey=FunctionName,ParameterValue=$FunctionName \
  --capabilities CAPABILITY_IAM
 
 echo "Waiting for resources to provision (this may take up to 30 minutes)..."
