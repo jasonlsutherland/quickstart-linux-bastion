b7ee6f25d0e5b86aaa067a5539c7368cc5fa1478
Jason Sutherland <jason.sutherland@Jason-Sutherlands-MacBook-Pro.local>
2020-01-31 16:19:40 +0000

    Updated for WordPress in lieu of bastion services.

--- .blueprint/deploy.sh
+++ .blueprint/deploy.sh
@@ -5,7 +5,7 @@ AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}
 echo "Submitting CloudFormation Template..."
 
 aws cloudformation create-stack --stack-name ${STACK_NAME} \
---template-body file://blueprint/templates/linux-wordpress-master.yaml \
+--template-body file://blueprint/templates/linux-wordpress-master.template \
  --capabilities CAPABILITY_IAM
 
 
